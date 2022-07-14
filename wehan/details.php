2e6cff21985a9f49459c49515ab22cab66e5ee9f
We-Han <chen007@live.at>
2022-07-14 14:08:45 +0200

    up

--- wehan/details.php
+++ wehan/details.php
@@ -1,3 +1,30 @@
+<?php
+require_once "actions/db_connect.php";
+require_once "components/boot.php";
+if(isset($_GET["id"])){
+  $sql = "SELECT * from products WHERE id= {$_GET['id']}";
+}
+
+$result = mysqli_query($connect, $sql);
+$tbody = "";
+$row = mysqli_fetch_assoc($result);
+// $info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+// var_dump_pretty($info);
+// if (mysqli_num_rows($result) > 0) {
+//     while ($row = mysqli_fetch_assoc($result)) {
+//         $tbody .= "<tr>
+//             <td><img class='img-thumbnail' src='pictures/" . $row['picture'] . "'</td>
+//            <td>" . $row['name'] . "</td>
+//            <td>" . $row['price'] . "€" . "</td>
+//            <td><a href='details.php?id=" . $row['id'] . "'><button class='btn btn-warning btn-sm' type='button'>Details</button></a><a href='update.php?id=" . $row['id'] . "'><button class='btn btn-primary btn-sm' type='button'>Edit</button></a>
+//            <a href='delete.php?id=" . $row['id'] . "'><button class='btn btn-danger btn-sm' type='button'>Delete</button></a></td>
+//             </tr>";
+//     }
+// } else {
+//     $tbody = "<tr><td colspan='5'><center>No Data Available </center></td></tr>";
+// }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +35,7 @@
 </head>
 <body>
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="pictures/<?= $row["picture"] ?>"  class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
